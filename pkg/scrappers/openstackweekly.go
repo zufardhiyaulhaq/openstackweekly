f546c6507cdba595e147e1d324e5af3296c11e09
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2020-10-31 06:16:21 +0100

    add super user news scrapper

--- pkg/scrappers/openstackweekly.go
+++ pkg/scrappers/openstackweekly.go
@@ -12,6 +12,7 @@ type OpenstackWeekly struct{}
 
 func (s *OpenstackWeekly) GetOpenStackWeekly(currentContent models.OpenStackContents) models.OpenStackContents {
 	content := getOpenStackNews(currentContent)
+	content = getOpenStackSuperUserNews(content)
 
 	return content
 }
@@ -45,6 +46,41 @@ func getOpenStackNews(currentContent models.OpenStackContents) models.OpenStackC
 		if doAppend {
 			content := models.Content{
 				Title: title, Url: url, Kind: "OpenStack News", IsDelivered: false,
+			}
+			currentContent.Content = append(currentContent.Content, content)
+		}
+	})
+
+	return currentContent
+}
+
+func getOpenStackSuperUserNews(currentContent models.OpenStackContents) models.OpenStackContents {
+	res, err := http.Get("https://superuser.openstack.org/")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".article-card").Each(func(i int, data *goquery.Selection) {
+		url, _ := data.Find("a").Attr("href")
+		title := data.Find("h3").Text()
+		doAppend := true
+
+		for _, v := range currentContent.Content {
+			if v.Url == url {
+				doAppend = false
+			}
+		}
+
+		if doAppend {
+			content := models.Content{
+				Title: title, Url: url, Kind: "OpenStack Super User News", IsDelivered: false,
 			}
 			currentContent.Content = append(currentContent.Content, content)
 		}
